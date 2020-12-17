cc560dd70dc4981e4a0b85f20db1343b3f4e1ac1
Roney Berti <33360638+roneyword@users.noreply.github.com>
2020-12-17 19:54:06 -0300

    ajustes da função de mensagem pelo whats e nova funcionalidade da mensagem customizada

--- js/modules/verify_dispositivo.js
+++ js/modules/verify_dispositivo.js
@@ -1,23 +1,40 @@
 export default function verifyDispositivo() {
-  const linkWhats = document.querySelectorAll("a");
+  const linkWhats = document.querySelectorAll("[data-contato]");
+  const inputCustomMsgWhats = document.querySelector("#mensageWhats");
+  const btnCustomWhats = document.querySelector("#customMsg");
+  let isMobile = false;
+  const mensage = "Olá! Gostaria de mais informações sobre seus serviços";
   const linkDesk =
-    "https://web.whatsapp.com/send?phone=5511977418876&text=Olá!%20Gostaria%20de%20mais%20informações%20sobre";
+    "https://web.whatsapp.com/send?phone=5511977418876&text=" + replaceWhiteSpace(mensage);
   const linkMobile =
-    "https://wa.me/5511977418876?text=Olá!%20Gostaria%20de%20mais%20informações%20sobre";
+    "https://wa.me/5511977418876?text=" + replaceWhiteSpace(mensage);
 
-  function linkDispositivo(link) {
-    linkWhats.forEach((element) => {
-      if (element.attributes[0].name == "data-contato") {
-        element.attributes.href.textContent = link;
-      }
-    });
+  function replaceWhiteSpace(msg){
+    return msg.replaceAll(' ', '%20');
   }
+  
+  btnCustomWhats.addEventListener('click', () => {
+    let msgCustomerWhats = inputCustomMsgWhats.value;
+    isMobile ?
+    btnCustomWhats.attributes.href.textContent = "https://wa.me/5511977418876?text=" + replaceWhiteSpace(msgCustomerWhats) :
+    btnCustomWhats.attributes.href.textContent = "https://web.whatsapp.com/send?phone=5511977418876&text=" + replaceWhiteSpace(msgCustomerWhats);
+    inputCustomMsgWhats.value = "";
+  });
 
-  if (screen.width < 640 || screen.height < 480) {
-    linkDispositivo(linkMobile);
-  } else if (screen.width < 1024 || screen.height < 768) {
-    linkDispositivo(linkMobile);
-  } else {
-    linkDispositivo(linkDesk);
+  if(linkWhats.length > 0) {
+
+    function linkDispositivo(link) {
+      linkWhats.forEach((element) => {
+        element.attributes.href.textContent = link;
+      });
+    }
+
+    if(screen.width < 1024 || screen.height < 768) {
+      linkDispositivo(linkMobile);
+      isMobile = true;
+    } else {
+      linkDispositivo(linkDesk);
+      isMobile = false;
+    }
   }
 }
